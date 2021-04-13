90bf22ca0b399600e28e2b48c626f25d7d6d0214
C1200 <coreyw.ultimatemedia@gmail.com>
2021-04-13 15:04:41 +0100

    Github Pages

---
+++ data.js
@@ -0,0 +1,15 @@
+var album = {
+    title: "C1200's Photo Album",
+
+    collections: [
+        {
+            name: "RodzCraft",
+            photos: [
+                { caption: "Me being patriotic", url: "images/2020-12-31_18.32.55.png" },
+                { caption: "Main Town with shaders", url: "images/2021-01-23_12.33.33.png" },
+                { caption: "RodzCraft's grave", url: "images/2021-04-10_17.18.38.png" },
+                { caption: "The book from the grave", url: "images/thegoodbyebook.png" }
+            ]
+        }
+    ]
+}
