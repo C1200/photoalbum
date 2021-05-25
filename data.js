ca501f23a23ed97c4aafab584b6a3559f177ffe5
C1200 <40738690+C1200@users.noreply.github.com>
2021-05-25 19:06:28 +0100

    Update data.js

--- data.js
+++ data.js
@@ -10,6 +10,14 @@ var album = {
                 { caption: "RodzCraft's grave", url: "images/2021-04-10_17.18.38.png" },
                 { caption: "The book from the grave", url: "images/thegoodbyebook.png" }
             ]
-        }
+        },
+        {
+            name: "My F*ck Ups",
+            photos: [
+                { caption: "m a t h s (pt1)", url: "images/hhdg564.png" },
+                { caption: "m a t h s (pt2)", url: "images/hgfd654.png" },
+                { caption: "wwwwwwwwwwww", url: "images/ofpd987.png" },
+                { caption: "2 disc channels", url: "images/whja798.png" },
+                { caption: "2 emojis in one reaction?", url: "images/trey654.png" },
     ]
 }
